1afe1d52320e4fda00a6edd39de9e8c84b931a1b
BlueMaxv2 <kriventsovmax48@yandex.ru>
2023-01-09 20:00:19 +0300

    First commit

--- program.py
+++ program.py
@@ -1,7 +1,28 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import pygame
+
+pygame.init()
+size = width, height = 612, 900
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Flappy Game')
+clock = pygame.time.Clock()
+FPS = 60
+bg_pos = 0
+bg_speed = 4
+bg_1 = pygame.image.load('data/background.png')
+bg_2 = pygame.image.load('data/background.png')
+sky = pygame.Surface([612, 533])
+sky.fill(pygame.Color('#3ec9ff'))
 
 
-if __name__ == '__main__':
-    main()
+running = True
+while running:
+    clock.tick(FPS)
+    screen.blit(bg_1, (bg_pos, 533))
+    bg_pos -= bg_speed
+    screen.blit(sky, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+pygame.quit()
