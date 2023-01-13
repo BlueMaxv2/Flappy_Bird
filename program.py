f2a2ffcbbe39ab524858658fc5c0b340ae73f443
BlueMaxv2 <kriventsovmax48@yandex.ru>
2023-01-13 16:49:47 +0300

    Second commit

--- program.py
+++ program.py
@@ -12,11 +12,21 @@ bg = pygame.image.load('data/ground.png')
 sky = pygame.Surface([612, 803])
 sky.fill(pygame.Color('#3ec9ff'))
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('data/bird.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+bird_group = pygame.sprite.Group()
+bird = Bird(100, 450)
+bird_group.add(bird)
 
 running = True
 while running:
     clock.tick(FPS)
     screen.blit(bg, (bg_pos, 800))
+    bird_group.draw(screen)
     bg_pos -= bg_speed
     if bg_pos < -40:
         bg_pos = 0
