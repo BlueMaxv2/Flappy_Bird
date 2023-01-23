d920e2626e69b9d367c7867affcc9470f15f176e
BlueMaxv2 <kriventsovmax48@yandex.ru>
2023-01-23 19:04:17 +0300

    Fifth commit Изменен спрайт птицы, изменено расширение окна игры, добавлены трубы

--- program.py
+++ program.py
@@ -1,17 +1,22 @@
 import pygame
+import random
 
 pygame.init()
-size = width, height = 612, 900
+size = width, height = 580, 500
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Flappy Game')
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 75
 bg_pos = 0
 bg_speed = 3
+game_over = False
+pipe_gap = 150
 
 bg = pygame.image.load('data/ground.png')
-sky = pygame.Surface([612, 803])
+sky = pygame.Surface([612, 403])
 sky.fill(pygame.Color('#00a2e8'))
+top_pipe_image = pygame.image.load('data/pipe.png')
+bottom_pipe_image = pygame.transform.flip(top_pipe_image, False, True)
 
 
 class Bird(pygame.sprite.Sprite):
@@ -33,7 +38,7 @@ class Bird(pygame.sprite.Sprite):
         self.fma += 0.5
         if self.fma > 9:
             self.fma = 9
-        if self.rect.bottom < 794:
+        if self.rect.bottom < 394:
             self.rect.y += int(self.fma)
         if pygame.mouse.get_pressed()[0] == 1 and self.pressed is False:
             self.fma -= 12
@@ -51,21 +56,52 @@ class Bird(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
 
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, flipped):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('data/pipe.png')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        if flipped == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if flipped == 0:
+            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+
+    def update(self):
+        self.rect.x -= bg_speed
+        if self.rect.x <= -width:
+            self.kill()
+
+
 b_group = pygame.sprite.Group()
-bird = Bird(100, 450)
+p_group = pygame.sprite.Group()
+bird = Bird(100, height / 2)
 b_group.add(bird)
+counter = 0
 
 running = True
 while running:
     clock.tick(FPS)
-    screen.blit(bg, (bg_pos, 800))
+    screen.blit(sky, (0, 0))
+    p_group.draw(screen)
+    p_group.update()
+    screen.blit(bg, (bg_pos, 400))
+    if not game_over:
+        bg_pos -= bg_speed
+        timer = 120
+        counter += 1
+        pipe_random = random.randint(0, 150)
+        if counter > timer:
+            counter = 0
+            bot_pipe = Pipe(650, int(height / 2) - pipe_random, 0)
+            top_pipe = Pipe(650, int(height / 2) - pipe_random, 1)
+            p_group.add(top_pipe)
+            p_group.add(bot_pipe)
+        if bg_pos < -40:
+            bg_pos = 0
     b_group.draw(screen)
     b_group.update()
-    bg_pos -= bg_speed
-    if bg_pos < -40:
-        bg_pos = 0
-    screen.blit(sky, (0, 0))
-    b_group.draw(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
